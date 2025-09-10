b4a0fb9e40109f236f5ef11f7a8d7d29da6fe043
jesus <yixux123@gmail.com>
2025-09-09 20:39:43 -0600

    Initial commit

---
+++ src/inicioDeSesion.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class inicioDeSesion {
+    private JPanel superPanel;
+    private JPanel panelCentral;
+    private JTextField emailText;
+    private JTextField contraseñaText;
+    private JCheckBox recordarCheck;
+    private JButton signInButton;
+    private JLabel Logo;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
